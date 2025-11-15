016c854efd0a2eeb55fb3b6f5a05e4e54d93a670
Unreallx <artemx2224@yandex@gmail.com>
2025-11-15 22:52:22 +0300

    add counter widget receiver example

--- app/src/main/java/com/unreallx/widgetforge/CounterWidgetReceiver.kt
+++ app/src/main/java/com/unreallx/widgetforge/CounterWidgetReceiver.kt
@@ -3,6 +3,6 @@ package com.unreallx.widgetforge
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
-class MyWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = MyGlanceWidget()
+class CounterWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = CounterWidget()
 }
